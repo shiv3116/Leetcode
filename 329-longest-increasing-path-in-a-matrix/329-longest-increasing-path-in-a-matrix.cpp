b30fb9acbadef00e2251a365249e11e874620532
Shivam Sharma <60747392+shiv3116@users.noreply.github.com>
2022-05-20 03:49:39 +0530

    Time: 402 ms (7.88%), Space: 28.5 MB (17.43%) - LeetHub

---
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int ans = INT_MIN;
+    int dfs(int i,int j,map<pair<int,int>,int> &dp,int prev,int n,int m,vector<vector<int>> &matrix)
+    {
+        if(i<0 || i==n || j<0 || j==m || matrix[i][j] <= prev)
+        {
+            return 0;
+        }
+        
+        if(dp[{i,j}]>0)
+        {
+            return dp[{i,j}];
+        }
+        
+        int res = 1;
+        res = max(res,1+dfs(i+1,j,dp,matrix[i][j],n,m,matrix));
+        res = max(res,1+dfs(i-1,j,dp,matrix[i][j],n,m,matrix));
+        res = max(res,1+dfs(i,j+1,dp,matrix[i][j],n,m,matrix));
+        res = max(res,1+dfs(i,j-1,dp,matrix[i][j],n,m,matrix));
+        dp[{i,j}] = res;
+        return res;
+    }
+    int longestIncreasingPath(vector<vector<int>>& matrix) {
+        int n = matrix.size(), m = matrix[0].size();
+        map<pair<int,int>,int> dp;
+        int ans;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+               ans = max(ans,dfs(i,j,dp,-1,n,m,matrix));
+            }
+        }
+        for(auto x:dp)
+        {
+            cout<<x.second<<" ";
+        }
+        return ans;
+    }
+};
