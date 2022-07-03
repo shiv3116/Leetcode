d0960a8db39b75beaf502fc3ff2e1cce2275cd30
Shivam Sharma <60747392+shiv3116@users.noreply.github.com>
2022-07-03 17:11:14 +0530

    Time: 38 ms (6.82%), Space: 7.9 MB (6.27%) - LeetHub

--- 376-wiggle-subsequence/376-wiggle-subsequence.cpp
+++ 376-wiggle-subsequence/376-wiggle-subsequence.cpp
@@ -4,10 +4,7 @@ public:
     {
         if(i==(int)nums.size())
         {
-            // for(auto x:tmp)
-            //     cout<<x<<" ";
             ans = max(ans,(int)tmp.size());
-            // cout<<'\n';
             return;
         }
         if(!tmp.empty() && tmp.back()>=0 && nums[i]-nums[i-1]>=0)
@@ -20,16 +17,7 @@ public:
         solve(nums,i+1,tmp,ans);
     }
     int wiggleMaxLength(vector<int>& nums) {
-        int cnt = 1;
-        for(int i=1;i<nums.size();i++)
-        {
-            if(nums[i]==nums[0])
-                cnt++;
-        }
-        if(cnt==nums.size())
-            return 1;
         vector<int> vals;
-        int f = 0;
         for(int i=0;i<nums.size();i++)
         {
             if(vals.empty())
@@ -39,8 +27,6 @@ public:
         }
         if(vals.back()!=nums[nums.size()-1])
             vals.push_back(nums[nums.size()-1]);
-        // for(auto x:vals)
-        //     cout<<x<<" ";
         int ans = 0,len=0;
         vector<int> tmp;
         solve(vals,1,tmp,ans);
