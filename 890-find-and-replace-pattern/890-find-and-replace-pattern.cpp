c953512b07eecbdadc62032a9880b95ebdafe3a4
Shivam Sharma <60747392+shiv3116@users.noreply.github.com>
2022-07-30 00:25:43 +0530

    Time: 0 ms (100.00%), Space: 8.2 MB (77.06%) - LeetHub

--- 890-find-and-replace-pattern/890-find-and-replace-pattern.cpp
+++ 890-find-and-replace-pattern/890-find-and-replace-pattern.cpp
@@ -2,15 +2,16 @@ class Solution {
 public:
     vector<string> findAndReplacePattern(vector<string>& words, string pattern) {
         vector<string> ans;
+        string pm = "";
         unordered_map<char,int> m;
         for(int i=0;i<pattern.size();i++)
         {
-            m[pattern[i]]++;
-        }
-        string pm = "";
-        for(int i=0;i<pattern.size();i++)
-        {
-            pm += (char)(m[pattern[i]]+'0');
+            if(m.find(pattern[i])!=m.end())
+            {
+                pm += (char)(m[pattern[i]]+'0');
+            }
+            pm += (char)(i+'0');
+            m[pattern[i]] = i;
         }
         m.clear();
         for(int i=0;i<words.size();i++)
@@ -18,16 +19,16 @@ public:
             string tmp = "";
             for(int j=0;j<words[i].size();j++)
             {
-                m[words[i][j]]++;
-            }
-            for(int j=0;j<words[i].size();j++)
-            {
-                tmp += (char)(m[words[i][j]]+'0');
+                if(m.find(words[i][j])!=m.end())
+                {
+                    tmp += (char)(m[words[i][j]]+'0');
+                }
+                tmp += (char)(j+'0');
+                m[words[i][j]] = j;
             }
             if(tmp==pm)
             {
-                if(words[i]!="yyxx")
-                    ans.push_back(words[i]);
+                ans.push_back(words[i]);
             }
             m.clear();
         }
