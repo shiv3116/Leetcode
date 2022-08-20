1a7a88ad4fe25182a95ca49d1ddaf98f8a6801a9
Shivam Sharma <60747392+shiv3116@users.noreply.github.com>
2022-08-20 21:08:37 +0530

    Time: 14 ms (10.72%), Space: 12.1 MB (74.79%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -4,22 +4,25 @@ public:
         int n = nums.size(),i;
         for(i=n-1;i>0;i--)
         {
-            if(nums[i-1]<nums[i]){
-                //swap(nums[i],nums[i-1]);
+            if(nums[i-1]<nums[i]) // finding the breakpoint
+            {
                 break;
             }
         }
-        if(i>=1 && nums[i-1]<nums[i]){
-            //cout<<"YES";
+        if(i>=1 && nums[i-1]<nums[i]) //To check whether a breakpoint exist or not
+        {
+            //If breakpoint exist
             int k = n-1;
-            while(nums[k]<=nums[i-1])
+            while(nums[k]<=nums[i-1]) // Finding the number just greater than the breakpoint
             {
                 k--;
             }
-            swap(nums[k],nums[i-1]);
-            sort(nums.begin()+i,nums.end());}
-        else{
-            sort(nums.begin(),nums.end());
+            swap(nums[k],nums[i-1]); //swapping it
+            reverse(nums.begin()+i,nums.end());//sorting it to get the smallest permutation(reverse can also be used)
+        }
+        else
+        {
+            reverse(nums.begin(),nums.end()); // If no breakpoint exist
         }
     }
 };
