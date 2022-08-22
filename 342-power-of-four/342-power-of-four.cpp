7921375a0e806f1ba6035e4ca4431b65ffdced12
Shivam Sharma <60747392+shiv3116@users.noreply.github.com>
2022-08-22 23:20:12 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (88.37%) - LeetHub

--- 342-power-of-four/342-power-of-four.cpp
+++ 342-power-of-four/342-power-of-four.cpp
@@ -3,18 +3,6 @@ public:
     bool isPowerOfFour(int n) {
         if(n<=0)
             return false;
-        int f = 0;
-        while(n>1)
-        {
-            if(n%4!=0)
-            {
-                f = 1;
-                break;
-            }
-            n /= 4;
-        }
-        if(f==1)
-            return false;
-        return true;
+        return (((n & (n-1))==0) && ((n-1)%3==0));
     }
 };
