d264be3a84524dabf531cc55e7220e509b2e6664
Shivam Sharma <60747392+shiv3116@users.noreply.github.com>
2022-09-18 02:39:27 +0530

    Time: 4 ms (34.58%), Space: 6 MB (46.25%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        if(x==2 && n==INT_MIN)
+        if(x>=2 && n==INT_MIN)
             return (double)(0.0);
         int p = abs(n);
         double ans = 1.0;
