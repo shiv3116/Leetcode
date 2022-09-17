d4e41587e8c47660fff1ce8e989fc8a0f7648459
Shivam Sharma <60747392+shiv3116@users.noreply.github.com>
2022-09-18 02:39:11 +0530

    Time: 4 ms (34.58%), Space: 5.9 MB (46.25%) - LeetHub

---
+++ 50-powx-n/50-powx-n.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if(x==2 && n==INT_MIN)
+            return (double)(0.0);
+        int p = abs(n);
+        double ans = 1.0;
+        while(p>0)
+        {
+            if(p & 1)
+            {
+                ans *= x;
+                p -= 1;
+            }
+            else
+            {
+                x *= x;
+                p /= 2;
+            }
+        }
+        if(n<0)
+            ans = (double)1.0/(double)ans;
+        return ans;
+    }
+};
